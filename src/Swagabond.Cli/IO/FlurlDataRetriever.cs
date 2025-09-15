66d4032e128f2499e18e088eb7fc5350e65231d3
Jordan Bleu <jordanbleu1@gmail.com>
2025-09-15 16:37:57 -0500

    drop requirement for .yaml or .json extension (#57)

--- src/Swagabond.Cli/IO/FlurlDataRetriever.cs
+++ src/Swagabond.Cli/IO/FlurlDataRetriever.cs
@@ -4,11 +4,6 @@ namespace Swagabond.Cli.IO;
 
 public class FlurlDataRetriever 
 {
-    public Task<Stream> GetDataStream(string input)
-    {
-        if (!input.EndsWith("json", StringComparison.OrdinalIgnoreCase) && !input.EndsWith("yaml", StringComparison.OrdinalIgnoreCase))
-            throw new UriFormatException("Url should end in .json or .yaml");
-
-        return input.GetStreamAsync(HttpCompletionOption.ResponseContentRead);
-    }
+    public Task<Stream> GetDataStream(string input) =>
+        input.GetStreamAsync(HttpCompletionOption.ResponseContentRead);
 }
